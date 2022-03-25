56797d22a0cb3241020e4370fe54a4f9db6064b1
Ray <fgprodigal@gmail.com>
2022-03-25 17:35:14 +0800

    Update blockp2p

--- luci-app-blockp2p/luasrc/controller/blockp2p.lua
+++ luci-app-blockp2p/luasrc/controller/blockp2p.lua
@@ -2,17 +2,5 @@
 module("luci.controller.blockp2p", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/blockp2p") then return end
-    entry({"admin", "network", "blockp2p"}, cbi("blockp2p"), _("Block P2P"), 60).dependent =
-        true
-
-    entry({"admin", "network", "blockp2p", "status"}, call("action_status"))
-end
-
-local function isrunning() return
-    luci.sys.call("pgrep blockp2p >/dev/null") == 0 end
-
-function action_status()
-    luci.http.prepare_content("application/json")
-    luci.http.write_json({run_state = isrunning()})
+    entry({"admin", "network", "blockp2p"}, cbi("blockp2p"), _("Block P2P"), 60)
 end

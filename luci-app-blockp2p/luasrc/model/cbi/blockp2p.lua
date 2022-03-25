f1649e6a35c0a8d0d745a360c5fd3b1237440441
Ray <fgprodigal@gmail.com>
2022-03-25 11:04:08 +0800

    Update blockp2p

--- luci-app-blockp2p/luasrc/model/cbi/blockp2p.lua
+++ luci-app-blockp2p/luasrc/model/cbi/blockp2p.lua
@@ -1,5 +1,4 @@
 mp = Map("blockp2p", translate("Block P2P"))
-mp:append(Template("blockp2p/status"))
 
 s = mp:section(TypedSection, "settings")
 s.anonymous = true
@@ -27,6 +26,8 @@ local apply = luci.http.formvalue("cbi.apply")
 if apply then
     luci.sys.exec("rm -f /tmp/iptables_nat_sum")
     luci.sys.exec("rm -f /tmp/iptables_mangle_sum")
+    luci.sys.exec("/etc/init.d/blockp2p enable")
+    luci.sys.exec("/etc/init.d/blockp2p restart")
 end
 
 return mp

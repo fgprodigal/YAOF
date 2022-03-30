92072305d93c8a1c90e814c52eb7b9b929e69bcc
Ray <fgprodigal@gmail.com>
2022-03-30 16:41:17 +0800

    Update

--- SCRIPTS/diy.sh
+++ SCRIPTS/diy.sh
@@ -4,4 +4,8 @@
 
 cp 999-ray-default-settings package/base-files/files/etc/uci-defaults/999-ray-default-settings
 
-sed -i 's/\%C/\%C build by Ray/g' package/base-files/files/etc/banner
+# sed -i 's/redirect_https\t1/redirect_https\t0/g' package/network/services/uhttpd/files/uhttpd.config
+
+sed -i 's/\%C/\%C compiled by Ray/g' package/base-files/files/etc/banner
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168./10.0./g' package/base-files/files/bin/config_generate
